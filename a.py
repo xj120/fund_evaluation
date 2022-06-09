3fbd87cdeca29c2bc3bec7dc48bacad0699c40fd
111 <702350138@qq.com>
2022-06-09 21:16:46 +0800

    函数改变

--- a.py
+++ a.py
@@ -11,68 +11,42 @@ import json
 
 app = Flask(__name__)
 
-#数据库信息
+"""#数据库信息
 conn = pymysql.connect(
     host='127.0.0.1',
     user='root',
     password='123456',
     db='Portfolio_Evaluation',
     charset='utf8'
-)
+)"""
 app.debug = True
 
 
 @app.route('/')
 def index2():
-    cur = conn.cursor()
-    sql = "select url from fund"
-    cur.execute(sql)
-    content = cur.fetchall()
-    return render_template("index.html", content = content)
+
+    #content：url的字典
+    return render_template("index.html",content = content)
 
 
-@app.route('/delete/<path:i>')
+"""@app.route('/delete/<path:i>')
 def delete(i):
     print(type(i))
     sql = 'delete from fund where url = ' + i;
     cur2 = conn.cursor()
     cur2.execute(sql)
     conn.commit()
-    return redirect("/")
-
-
-
-#爬取一个URL，i[1]是URL
-"""@app.route('/spide/<i>')
-def spurl(i):
- BackEnd.crawler.geturlInfo(i[1])
- cur4 = conn.cursor()
- sql="update url  set found_date=" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) +"where url=" + i[1];
- cur4.execute(sql)
- conn.commit();
- return redirect("/")
-
-#爬取全部URL，content是全部记录，i是单条记录，i[1]是URL
-@app.route('/spideall/<content>')
-def spideall(content):
-    for i in content:
-        BackEnd.crawler.geturlInfo(i[1])
-        cur4 = conn.cursor()
-        sql = "update url  set found_date=" + time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) + "where url=" + i[1];
-        cur4.execute(sql)
-        conn.commit();
     return redirect("/")"""
 
-@app.route('/add', methods=["POST"])
-def add():
-    Url = request.form.get("URL")
-    id = request.form.get("id")
-    time = datetime.datetime.today()
-    sql = "insert into fund(number,url) values(" + id + "," + Url + ")"
-    cur3 = conn.cursor()
-    cur3.execute(sql)
-    conn.commit()
-    return redirect("/")
+
+
+#
+@app.route('/spide', methods=["POST"])
+def spide():
+    #爬虫程序：
+ a = request.form.get('url')
+ print(a)
+ return redirect('/')
 
 
 if __name__ == "__main__":
