7a1a4aef4f652a3a5b6d549ce411bc1de78ff194
FhawkII <2695435309@qq.com>
2022-06-10 14:53:42 +0800

    新增相关性研究的面板； 修改了数据表格：新增两列； 调整了折线图图例位置

--- static/js/DataTable.js
+++ static/js/DataTable.js
@@ -8,15 +8,17 @@ $(function () {
             , url: '../static/data/table.json'
             , height: 510
             , toolbar: true
-            , page: false //开启分页
+            , page: false //分页
             , cols: [[ //表头
-                {field: 'v_id', title: '大V名字', width: 130, fixed: 'left'}
-                , {field: 'group_id', title: '组合名字', width: 130}
-                , {field: 'gains', title: '涨幅(%)', width: 160, sort: true}
-                , {field: 'max_retracement', title: '最大回撤(%)', width: 160, sort: true}
-                , {field: 'sharpe_ratio', title: '夏普率(%)', width: 160, sort: true}
-                , {field: 'annualized_volatility', title: '年化波动率(%)', width: 160, sort: true}
-                , {field: 'fans_num', title: '粉丝数(人)', width: 170, sort: true}
+                {field: 'v_id', title: '组合号', width: 110, fixed: 'left'}
+                , {field: 'group_id', title: '组合名字', width: 170}
+                , {field: 'gains', title: '涨幅(%)', width: 120, sort: true}
+                , {field: 'max_retracement', title: '最大回撤(%)', width: 140, sort: true}
+                , {field: 'sharpe_ratio', title: '夏普率(%)', width: 120, sort: true}
+                , {field: 'annualized_volatility', title: '年化波动率(%)', width: 150, sort: true}
+                , {field: 'fans_num', title: '粉丝数(人)', width: 130, sort: true}
+                , {field: 'hold_time', title: '持有时间()', width: 130, sort: true}
+                , {field: 'adjust_level', title: '调仓水平', width: 130, sort: true}
             ]]
         });
     });
