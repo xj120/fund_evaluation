9a6cd871363e83ffe3cfd97bf0e2cacfd236f9c7
FhawkII <2695435309@qq.com>
2022-07-01 01:13:08 +0800

    merge

--- static/js/DataTable.js
+++ static/js/DataTable.js
@@ -10,14 +10,14 @@ $(function () {
             , toolbar: true
             , page: false //分页
             , cols: [[ //表头
-                {field: 'v_id', title: '大V ID', width: 100, fixed: 'left'}
-                , {field: 'group_id', title: '大V名字', width: 100}
+                {field: 'v_id', title: '组合ID', width: 100, fixed: 'left'}
+                , {field: 'manager_name', title: '大V名字', width: 100}
                 , {field: 'group_id', title: '组合名字', width: 100}
                 , {field: 'gains', title: '涨幅(%)', width: 100, sort: true}
                 , {field: 'max_retracement', title: '最大回撤(%)', width: 123, sort: true}
                 , {field: 'sharpe_ratio', title: '夏普率(%)', width: 110, sort: true}
                 , {field: 'annualized_volatility', title: '年化波动率(%)', width: 138, sort: true}
-                , {field: 'annualized_volatility', title: '年化收益率(%)', width: 138, sort: true}
+                , {field: 'rate_per_ann', title: '年化收益率(%)', width: 138, sort: true}
                 , {field: 'fans_num', title: '粉丝数(人)', width: 115, sort: true}
                 , {field: 'average_holding_time', title: '持有时间(月)', width: 125, sort: true}
                 , {field: 'reposition_level', title: '调仓水平', width: 110, sort: true}
